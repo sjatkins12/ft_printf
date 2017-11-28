5df5da8d4fb18a7308aece779ec2aa3ce6a79776
Samuel Atkins <sjatkins12@gmail.com>
2017-11-28 14:40:18 -0800

    trying to fix size_t

--- printf/num_format.c
+++ printf/num_format.c
@@ -48,7 +48,7 @@ long long int				signed_len(va_list *ap, t_flag arg_flag)
 	else if (arg_flag.e_length == j)
 		num = (intmax_t)va_arg(*ap, long long int);
 	else if (arg_flag.e_length == z)
-		num = (int)va_arg(*ap, long long int);
+		num = (signed char)va_arg(*ap, long long int);
 	else
 		num = (int)va_arg(*ap, long long int);
 	return (num);
